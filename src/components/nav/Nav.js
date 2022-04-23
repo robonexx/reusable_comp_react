6c897a4ba55e14cffea86c9a00fc0c2b55a649c6
robonexx <robertwagar@gmail.com>
2022-04-23 21:57:02 +0200

    framer nav animations

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+
 
 // styles
 import styles from '../../styles/Nav.module.css';
@@ -14,22 +17,40 @@ const navItems = [
     url: '/components',
   },
   {
+    title: 'page3',
+    url: '/',
+  },
+  {
+    title: 'page4',
+    url: '/',
+  },
+  {
     title: 'contact',
     url: '/contact',
   },
 ];
+
+
 
 export default function Nav() {
   return (
     <>
       <nav className={styles.nav}>
         <ul className={styles.nav_menu}>
-          {navItems.map(({ title, url }) => (
-            <li className={styles.nav_item} key={title}>
+          {navItems.map(({ title, url }, i) => (
+            <motion.li className={styles.nav_item} key={title}
+              initial={{ opacity: 0, y: i % 2 === 0 ? -100 : 100 }}
+              animate={{ opacity: 1, y: 0}}
+              transition={{ duration: 1, delay: i * 0.2 }}
+              whileHover={{
+                scale: 1.2, rotate: 15,
+                transition: { duration: 0.2 }
+              }}
+            >
               <Link to={url} className={styles.nav_links}>
                 {title}
               </Link>
-            </li>
+            </motion.li>
           ))}
         </ul>
       </nav>
